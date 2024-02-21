818253183ae0487bb2786c7f898cebc65e32e69b
shitajha14 <shitaljha2312@gmail.com>
2024-02-21 16:17:17 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navbar'
 import About from './components/aboutus'
 import Hero from './components/hero'
+
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <Hero/>
       <About/>
-
+      
     </div>
   );
 }
