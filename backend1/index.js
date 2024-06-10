e4cdf58db79f577ed8ceab84d3e7461aa9b43c02
harshitnitjsr <harshit276092004@gmail.com>
2024-06-10 18:27:19 +0530

    Revert "blog"
    This reverts commit 3ee989148851056dc049a4a821305b965dce6181.

--- backend1/index.js
+++ backend1/index.js
@@ -6,27 +6,28 @@ const multer = require("multer");
 const path = require("path");
 const Signup = require("./routes/Signup");
 const Login = require("./routes/Login");
-const AddEvent = require("./routes/Addevents");
-const AuthRoute = require("./routes/authRoute");
-const Displayevents = require("./routes/Displayevents");
-const Members = require("./routes/Displaymembers");
+const AddEvent = require("./routes/Addevents")
+const AuthRoute=require("./routes/authRoute")
+const Displayevents = require("./routes/Displayevents")
+const Members = require("./routes/Displaymembers")
 const verifytoken = require("./routes/verifytoken");
 const bodyParser = require("body-parser");
-const Delete = require("./routes/Delete");
-const postRoutes = require("./routes/post.route");
+const Delete = require("./routes/Delete")
 const app = express();
 app.use(
   cors({
+
     origin: "*",
 
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+   
   })
 );
 // app.use('/userImages',express.static('userImages'))
 // app.use('/thumbnail',express.static('thumbnail'))
 
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 // app.use(express.static("public"))
 dotenv.config();
 // dotenv.config({ path: "./config/config.env" });
@@ -42,6 +43,9 @@ connectDB();
 //   })
 // })
 
+
+
+
 // const storage = multer.diskStorage({
 //   destination: function (req, file, cb) {
 //     cb(null, 'userImages/')
@@ -52,6 +56,12 @@ connectDB();
 // })
 
 // const upload = multer({ storage: storage })
+
+
+
+
+
+
 
 // const storage1 = multer.diskStorage({
 //   destination: function (req, file, cb) {
@@ -64,12 +74,15 @@ connectDB();
 
 // const upload1 = multer({ storage: storage1 })
 
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb" }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
+
+
 
 //app.use("/uploads", express.static(path.join(__dirname, "./uploads")));
 // app.use("/api/user", require("./routes/authRoute"));
@@ -77,14 +90,14 @@ app.use("/api/user", AuthRoute);
 app.use("/api/v1/", Signup);
 app.use("/api/v1/", Login);
 
-app.use("/api/v1/", Displayevents);
-app.use("/api/v1/", Members);
-app.use("/api/v1/", verifytoken);
-app.use("/api/v1/", AddEvent);
+app.use("/api/v1/",Displayevents );
+app.use("/api/v1/",Members );
+app.use("/api/v1/",verifytoken );
+app.use("/api/v1/",AddEvent);
 
-app.use("/api/v1/", Delete);
+app.use("/api/v1/",Delete);
 // app.use("/api/search", tokenCheck, searchRouter);
-app.use("/api/post", postRoutes);
+
 
 // app.use("/api/upload", tokenCheck, require("./routes/uploadRoutes"));
 //app.use("/api/upload", require("./routes/uploadRoutes"));
--- gdscnitjsr1/src/components/ourTeam.jsx
+++ gdscnitjsr1/src/components/ourTeam.jsx
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from "react";
-
+import Modal from "./Modal";
 import {
   Navigation,
   Pagination,
   Scrollbar,
   A11y,
+  Autoplay,
 } from "swiper/modules";
 import Leads from "./Leads";
 import styled from "styled-components";
 import { Swiper, SwiperSlide } from "swiper/react";
-
+import { FaGithub, FaInstagram, FaLinkedin, FaEnvelope } from 'react-icons/fa';
 import axios from "axios";
 
 import "swiper/css";
@@ -19,53 +20,54 @@ import "swiper/css/scrollbar";
 
 import Llogo from "../img/gdsc bracket left.svg";
 import Rlogo from "../img/gdsc bracket right.svg";
-import Example from "./link";
 
 const PersonCard = ({ person }) => {
+  const [isOpen, setOpen] = useState(false);
+
   return (
-    <>
+    <div>
       <img
         className="m-auto md:h-[300px] md:w-[300px] h-[225px] w-[225px] object-cover rounded-full cursor-pointer"
         src={person.imageurl}
         alt={person.name}
+        onClick={() => setOpen(true)}
       />
-      
-        <Example
-          name={person.name}
-          githublink={person.github}
-          instagramlink={person.instagram}
-          linkedinlink={person.linkedin}
-          facebooklink={person.facebook}
-          xlink={person.x}
-          gmail={person.mail}
-        />
-      
-    </>
+      <h2 className="text-center text-[32px] font-Patrick leading-[48px] cursor-pointer"  onClick={() => setOpen(true)}>
+        {person.name}
+      </h2>
+      <Modal
+        isOpen={isOpen}
+        setOpen={setOpen}
+        onClose={() => setOpen(false)}
+        name={person.name}
+        githublink={`${person.github}`}
+        instagramlink={`${person.instagram}`}
+        linkedinlink={`${person.linkedin}`}
+        facebooklink={`${person.facebook}`}
+        xlink={`${person.x}`}
+        gmail={`${person.mail}`}
+      />
+    </div>
   );
 };
 
 const OurTeam = () => {
   const [profiles, setProfiles] = useState([]);
-  const [swiperInstance, setSwiperInstance] = useState(null);
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchdata = async () => {
       try {
-        const response = await axios.get(
-          "https://gds-cnitjsr-officalweb.vercel.app/api/v1/displaymembers"
-        );
+        const response = await axios.get("https://gds-cnitjsr-officalweb.vercel.app/api/v1/displaymembers");
         setProfiles(response.data);
-
-        const registrationArray = response.data.map((person) => person.registration);
-        localStorage.setItem("registrationArray", JSON.stringify(registrationArray));
+        console.log(response.data)
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
-    fetchData();
+    fetchdata();
   }, []);
 
-  const slideNo = () => {
+  const slideno = () => {
     if (window.innerWidth >= 850) {
       return 3;
     } else if (window.innerWidth >= 580) {
@@ -76,46 +78,54 @@ const OurTeam = () => {
   };
 
   return (
-    <div className="container mx-auto px-4 mt-10 z-50 relative" id="our-team">
-      <div className="flex items-center justify-center">
-        <img src={Llogo} alt="left bracket logo" />
-        <h1 className="font-Patrick sm:text-[64px] text-[30px]">Our Team</h1>
-        <img src={Rlogo} alt="right bracket logo" />
+    <>
+      <div className="container mx-auto px-4 mt-10" id="our-team">
+        <div className="flex items-center justify-center">
+          <img src={Llogo} alt="left bracket logo" />
+          <h1 className="font-Patrick sm:text-[64px] text-[30px] "> Our Team</h1>
+          <img src={Rlogo} alt="right bracket logo" />
+        </div>
+        <div className="flex items-center justify-center mx-0">
+          <p className="sm:text-[24px] text-[16px] font-Lato  text-[#5F6368] self-center mt-[44px]">
+            Meet the incredible GDSC team - a synergy of coding brilliance, design
+            prowess, strategic minds, and innovation wizards. Together, they
+            propel the boundaries of technology.
+          </p>
+        </div>
+        <br />
+        <br />
+        <Leads />
+        <br />
+        <br />
+        <h1 className="font-Patrick sm:text-[48px] text-[24px] text-decoration-line: underline">
+          Meet the Core
+        </h1>{" "}
+        <br />
+        <br />
+        <Swiper
+          className="md:h-[440px] h-[350px]"
+          spaceBetween={50}
+          slidesPerView={slideno()}
+          navigation={true}
+          pagination={{
+            clickable: true,
+          }}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          modules={[Pagination, Navigation, Autoplay]}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+        >
+          {profiles.map((person, index) => (
+            <SwiperSlide key={index}>
+              <PersonCard person={person} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
-      <div className="flex items-center justify-center mx-0">
-        <p className="sm:text-[24px] text-[16px] font-Lato text-[#5F6368] self-center mt-[44px]">
-          Meet the incredible GDSC team - a synergy of coding brilliance, design
-          prowess, strategic minds, and innovation wizards. Together, they
-          propel the boundaries of technology.
-        </p>
-      </div>
-      <br />
-      <br />
-      <Leads />
-      <br />
-      <br />
-      <h1 className="font-Patrick sm:text-[48px] text-[24px] underline">
-        Meet the Core
-      </h1>
-      <br />
-      <br />
-      <Swiper
-        className="md:h-[440px] h-[350px]"
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
-        navigation
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
-        slidesPerView={slideNo()}
-        autoplay={{ delay: 0, disableOnInteraction: false }} // Disable autoplay
-        onSwiper={setSwiperInstance}
-      >
-        {profiles.map((person, index) => (
-          <SwiperSlide key={index}>
-            <PersonCard person={person} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>
+    </>
   );
 };
 
